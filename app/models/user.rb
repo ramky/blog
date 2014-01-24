fad37d45c521df8792603ec792b7d8337734a16b
Ram Iyer <ramky.iyer@yahoo.com>
2014-01-24 12:22:02 -0800

    adding m-2-m relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :post
+	has_many :user_groups
+	has_many :groups, through: :user_groups
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140124192133) do
+ActiveRecord::Schema.define(version: 20140124200213) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -23,12 +23,25 @@ ActiveRecord::Schema.define(version: 20140124192133) do
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
+  create_table "groups", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "user_groups", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
