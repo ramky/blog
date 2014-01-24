9bd4e8629f71764094d11d34552d1e64a3bb4b86
Ram Iyer <ramky.iyer@yahoo.com>
2014-01-24 10:57:24 -0800

    adding users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140124175646) do
+ActiveRecord::Schema.define(version: 20140124185501) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20140124175646) do
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
